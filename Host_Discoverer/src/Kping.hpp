44be2a75371566361c41241020ea9204dc4683d4
k1000 <75310214+KOneThousand@users.noreply.github.com>
2022-06-25 13:34:22 +0200

    Delete Host_Discoverer directory

--- Host_Discoverer/src/Kping.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef KPING_H
-#define KPING_H
-unsigned short checksum(void *b, int len);
-void intHandler(int var);
-char *dns_lookup(char *addr_host, struct sockaddr_in *addr_con);
-bool send_ping(char* ipToPing, int sockfd, struct ping_pkt &pckt, struct sockaddr_in const &r_addr);
-#endif
